4a759d752696a0be81ad45a45e06fd428bd8266d
Andy Bates <bates.andy123@gmail.com>
2016-10-07 12:13:41 -0700

    Got RTOS running

--- inc/FreeRTOSConfig.h
+++ inc/FreeRTOSConfig.h
@@ -79,7 +79,7 @@
 #define configTICK_RATE_HZ				( ( portTickType ) 1000 )
 #define configMAX_PRIORITIES			( 5 )
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 20 )
-#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 4 * 256 ) )
+#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 15 * 350 ) )
 #define configMAX_TASK_NAME_LEN			( 15 )
 #define configUSE_TRACE_FACILITY		1
 #define configUSE_16_BIT_TICKS			0
